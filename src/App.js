4714bf6e1fb862c35438fbf8f4dd926244bf4084
Alexandra Kon <kononenkoalexandra2002@gmail.com>
2024-03-22 00:38:41 +0100

    add changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
   updateDoc,
   deleteDoc,
   doc,
-  onSnapshot
+  onSnapshot,
 } from "firebase/firestore";
 
 import politico1 from "./Images/Politicos/politico1.jpg";
@@ -29,7 +29,6 @@ import politico13 from "./Images/Politicos/politico13.webp";
 import politico14 from "./Images/Politicos/politico14.jpg";
 import politico15 from "./Images/Politicos/politico15.jpg";
 
-
 import audioHimno from "./Sounds/Himno_URSS.mp3";
 import audioGta from "./Sounds/GTA_Mision.mp3";
 import audioCabra from "./Sounds/Cabra_Gritando.mp3";
@@ -39,13 +38,11 @@ import audioPedro from "./Sounds/Pedro_Piqueras.mp3";
 import audioTit from "./Sounds/Titanic_flute.mp3";
 import audioMatar from "./Sounds/Me_Matar.mp3";
 
-
-import audioGadget from "./Sounds/Inspector_Gadget.mp3"
+import audioGadget from "./Sounds/Inspector_Gadget.mp3";
 
 import audioExpediente from "./Sounds/Expediente_X.mp3";
 import audioMartin from "./Sounds/Martín_Matin.mp3";
 import audioPers from "./Sounds/persecución.mp3";
-
 
 function App() {
   return (
@@ -64,7 +61,7 @@ function Home() {
   const [newUser, setNewUser] = useState({
     name: "",
     points: 0,
-    señor: false
+    señor: false,
   });
   const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(db, "users");
@@ -84,7 +81,7 @@ function Home() {
     politico12,
     politico13,
     politico14,
-    politico15
+    politico15,
   ];
 
   const createUser = async () => {
@@ -95,13 +92,13 @@ function Home() {
       name: newUser.name,
       points: Number(newUser.points),
       señor: newUser.señor,
-      image: randomImage
+      image: randomImage,
     });
 
     setNewUser({
       name: "",
       points: 0,
-      señor: false
+      señor: false,
     });
   };
 
@@ -110,6 +107,15 @@ function Home() {
 
     const userDoc = doc(db, "users", id);
     const newFields = { points: points + pointsToAdd };
+    await updateDoc(userDoc, newFields);
+  };
+
+  const deletePointsUser = async (id, points) => {
+    const pointsToAdd =
+      parseInt(prompt(`¿Cuántos puntos desea eliminar?`)) || 0;
+
+    const userDoc = doc(db, "users", id);
+    const newFields = { points: points - pointsToAdd };
     await updateDoc(userDoc, newFields);
   };
 
@@ -137,8 +143,14 @@ function Home() {
   const juniors = users.filter((user) => !user.señor);
 
   // Calcular la suma de puntos para cada equipo
-  const totalPointsSeñores = señores.reduce((acc, curr) => acc + curr.points, 0);
-  const totalPointsJuniors = juniors.reduce((acc, curr) => acc + curr.points, 0);
+  const totalPointsSeñores = señores.reduce(
+    (acc, curr) => acc + curr.points,
+    0
+  );
+  const totalPointsJuniors = juniors.reduce(
+    (acc, curr) => acc + curr.points,
+    0
+  );
 
   return (
     <div>
@@ -198,14 +210,21 @@ function Home() {
                   updateUser(user.id, user.points);
                 }}
               >
-                Añadir Puntos
+                +
+              </button>
+              <button
+                onClick={() => {
+                  deletePointsUser(user.id, user.points);
+                }}
+              >
+                -
               </button>
               <button
                 onClick={() => {
                   deleteUser(user.id);
                 }}
               >
-                Delete User
+                Delete {user.name}
               </button>
             </div>
           ))}
@@ -228,24 +247,29 @@ function Home() {
                   updateUser(user.id, user.points);
                 }}
               >
-                Añadir Puntos
+                +
+              </button>
+              <button
+                onClick={() => {
+                  deletePointsUser(user.id, user.points);
+                }}
+              >
+                -
               </button>
               <button
                 onClick={() => {
                   deleteUser(user.id);
                 }}
               >
-                Delete User
+                Delete {user.name}
               </button>
             </div>
           ))}
         </div>
       </div>
-
     </div>
   );
 }
-
 
 function Sonidos() {
   const playSound = (audioId) => {
@@ -270,97 +294,125 @@ function Sonidos() {
           <div className="card">
             <img
               src="https://img.freepik.com/vector-premium/icono-peligro-radiacion-alerta-amenaza-radiactiva-simbolo-precaucion-nuclear_342166-429.jpg?w=2000"
-              onClick={() => playSound('Nuclear_audio')}
+              onClick={() => playSound("Nuclear_audio")}
               alt="Play Sound"
             />
             <audio id="Nuclear_audio" src={audioNuclear}></audio>
             <p>Alerta Nuclear</p>
-            <button onClick={() => pauseSound('Nuclear_audio')}>Stop</button>
+            <button onClick={() => pauseSound("Nuclear_audio")}>Stop</button>
           </div>
           <div class="card">
-            <img src="https://animapedia.org/wp-content/uploads/2019/07/cabra-1.jpg"
-              onClick={() => playSound('cabra_audio')} alt="Play Sound"/>
-              <audio id="cabra_audio" src={audioCabra}></audio>
-              <p>Grito Cabra</p>
-              <button onClick={() => pauseSound('cabra_audio')}>Stop</button>
+            <img
+              src="https://animapedia.org/wp-content/uploads/2019/07/cabra-1.jpg"
+              onClick={() => playSound("cabra_audio")}
+              alt="Play Sound"
+            />
+            <audio id="cabra_audio" src={audioCabra}></audio>
+            <p>Grito Cabra</p>
+            <button onClick={() => pauseSound("cabra_audio")}>Stop</button>
           </div>
           <div class="card">
-            <img src="https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fgrand-theft-auto-v%2Fhome%2FGTAV_EGS_Artwork_1920x1080_Hero-Carousel_V06-1920x1080-1503e4b1320d5652dd4f57466c8bcb79424b3fc0.jpg"
-              onClick={() => playSound('gta_audio')} alt="Play Sound"/>
-              <audio id="gta_audio" src={audioGta}></audio>
-              <p>GTA Mision Complete</p>
-              <button onClick={() => pauseSound('gta_audio')}>Stop</button>
+            <img
+              src="https://cdn2.unrealengine.com/Diesel%2Fproductv2%2Fgrand-theft-auto-v%2Fhome%2FGTAV_EGS_Artwork_1920x1080_Hero-Carousel_V06-1920x1080-1503e4b1320d5652dd4f57466c8bcb79424b3fc0.jpg"
+              onClick={() => playSound("gta_audio")}
+              alt="Play Sound"
+            />
+            <audio id="gta_audio" src={audioGta}></audio>
+            <p>GTA Mision Complete</p>
+            <button onClick={() => pauseSound("gta_audio")}>Stop</button>
           </div>
           <div class="card">
-            <img class="img_card"
+            <img
+              class="img_card"
               src="https://okdiario.com/img/2022/02/24/-que-paises-formaron-parte-de-la-urss_.jpg"
-              onClick={() => playSound('himnoUrss')} alt="Play Sound"/>
-              <audio id="himnoUrss" src={audioHimno}></audio>
-              <p>URSS</p>
-              <button  onClick={() => pauseSound('himnoUrss')}>Stop</button>
+              onClick={() => playSound("himnoUrss")}
+              alt="Play Sound"
+            />
+            <audio id="himnoUrss" src={audioHimno}></audio>
+            <p>URSS</p>
+            <button onClick={() => pauseSound("himnoUrss")}>Stop</button>
           </div>
           <div class="card">
-            <img class="img_card"
+            <img
+              class="img_card"
               src="https://i.ytimg.com/vi/Gotz1iO_w4E/maxresdefault.jpg"
-              onClick={() => playSound('matar')} alt="Play Sound"/>
-              <audio id="matar" src={audioMatar}></audio>
-              <p>Me voy a matar</p>
-              <button  onClick={() => pauseSound('matar')}>Stop</button>
+              onClick={() => playSound("matar")}
+              alt="Play Sound"
+            />
+            <audio id="matar" src={audioMatar}></audio>
+            <p>Me voy a matar</p>
+            <button onClick={() => pauseSound("matar")}>Stop</button>
           </div>
           <div class="card">
-            <img class="img_card"
+            <img
+              class="img_card"
               src="https://images.ecestaticos.com/c4hpdnLzRo8r-38mIYxZNVdheGk=/0x0:1481x843/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fa2a%2F41c%2Ff55%2Fa2a41cf555c504efd8b2be886bcf93ee.jpg"
-              onClick={() => playSound('piqueras')} alt="Play Sound"/>
-              <audio id="piqueras" src={audioPedro}></audio>
-              <p>Pedro Piqueras</p>
-              <button onClick={() => pauseSound('piqueras')}>Stop</button>
+              onClick={() => playSound("piqueras")}
+              alt="Play Sound"
+            />
+            <audio id="piqueras" src={audioPedro}></audio>
+            <p>Pedro Piqueras</p>
+            <button onClick={() => pauseSound("piqueras")}>Stop</button>
           </div>
           <div class="card">
-            <img class="img_card"
+            <img
+              class="img_card"
               src="https://historia.nationalgeographic.com.es/medio/2021/02/12/titanic_dd4b0e80_907x805.jpg"
-              onClick={() => playSound('titanic')} alt="Play Sound"/>
-              <audio id="titanic" src={audioTit}></audio>
-              <p>Titanic</p>
-              <button  onClick={() => pauseSound('titanic')}>Stop</button>
+              onClick={() => playSound("titanic")}
+              alt="Play Sound"
+            />
+            <audio id="titanic" src={audioTit}></audio>
+            <p>Titanic</p>
+            <button onClick={() => pauseSound("titanic")}>Stop</button>
           </div>
           <div class="card">
-            <img class="img_card"
+            <img
+              class="img_card"
               src="https://i1.wp.com/clipset.com/wp-content/uploads/2013/10/gadget.jpg?ssl=1"
-              onClick={() => playSound('ins_gad')} alt="Play Sound"/>
-              <audio id="ins_gad" src={audioGadget}></audio>
-              <p>Inspector Gadget</p>
-              <button onClick={() => pauseSound('ins_gad')}>Stop</button>
+              onClick={() => playSound("ins_gad")}
+              alt="Play Sound"
+            />
+            <audio id="ins_gad" src={audioGadget}></audio>
+            <p>Inspector Gadget</p>
+            <button onClick={() => pauseSound("ins_gad")}>Stop</button>
           </div>
           <div class="card">
-            <img class="img_card"
+            <img
+              class="img_card"
               src="https://pics.filmaffinity.com/Expediente_X_Serie_de_TV-450691206-large.jpg"
-              onClick={() => playSound('exp_x')} alt="Play Sound"/>
-              <audio id="exp_x" src={audioExpediente}></audio>
-              <p>Expediente X</p>
-              <button  onClick={() => pauseSound('exp_x')}>Stop</button>
+              onClick={() => playSound("exp_x")}
+              alt="Play Sound"
+            />
+            <audio id="exp_x" src={audioExpediente}></audio>
+            <p>Expediente X</p>
+            <button onClick={() => pauseSound("exp_x")}>Stop</button>
           </div>
           <div class="card">
-            <img class="img_card"
+            <img
+              class="img_card"
               src="http://1.bp.blogspot.com/-b4doarIZ2xM/U1PEG-G2bNI/AAAAAAAA4Vk/ey4TH2aj1Sg/s1600/martin10.jpg"
-              onClick={() => playSound('martin_martin')} alt="Play Sound"/>
-              <audio id="martin_martin" src={audioMartin}></audio>
-              <p>Martin Martin</p>
-              <button  onClick={() => pauseSound('martin_martin')}>Stop</button>
+              onClick={() => playSound("martin_martin")}
+              alt="Play Sound"
+            />
+            <audio id="martin_martin" src={audioMartin}></audio>
+            <p>Martin Martin</p>
+            <button onClick={() => pauseSound("martin_martin")}>Stop</button>
           </div>
           <div class="card">
-            <img class="img_card"
+            <img
+              class="img_card"
               src="https://www3.gobiernodecanarias.org/medusa/proyectos/proyectonewton/wp-content/uploads/sites/20/2018/12/males-2512006_640.jpg"
-              onClick={() => playSound('persecucion')} alt="Play Sound"/>
-              <audio id="persecucion" src={audioPers}></audio>
-              <p>Persecucion</p>
-              <button  onClick={() => pauseSound('persecucion')}>Stop</button>
+              onClick={() => playSound("persecucion")}
+              alt="Play Sound"
+            />
+            <audio id="persecucion" src={audioPers}></audio>
+            <p>Persecucion</p>
+            <button onClick={() => pauseSound("persecucion")}>Stop</button>
           </div>
-
         </div>
       </div>
     </div>
   );
 }
-
 
 export default App;
